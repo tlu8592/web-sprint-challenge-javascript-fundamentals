1c0ca536bc43554f7609c15bfc6c8a732fcdb276
Tony Lu <TonyLu@Tonys-iMac.fios-router.home>
2021-04-18 19:51:38 -0400

    Test passed for USApop function

--- index.js
+++ index.js
@@ -102,10 +102,12 @@ const zooAnimals = [
   Remember the reduce method takes two arguments: a callback (which itself takes two args - the accumulator and the item), and an initial value for the count.
   */
 
-  function USApop(/*Your Code Here*/){
-    /*Your Code Here*/
+  function USApop(array){
+    return array.reduce((acc, item) => {
+      return acc + item.population;
+    }, 0);
   }
-  
+  console.log(USApop(zooAnimals));
   
   // 🦁🦁🦁 Callbacks 🦁🦁🦁  
   /* 🦁🦁🦁 Step 1: Create a higher-order function 🦁🦁🦁
